972fcfde8139dae5a556ed7de4069b7a60e1ed43
Jon Daniel <binarycleric@gmail.com>
2016-03-04 21:04:19 -0500

    Accepts argument for file path

--- src/main.rs
+++ src/main.rs
@@ -1,49 +1,41 @@
-/*
+use std::env;
+use std::path::Path;
 use std::io::prelude::*;
 use std::fs::File;
 
-fn main() {
-    let mut f = File::open("Cargo.toml");
-    let mut s = String::new();
-
-    match File::open("Cargo.toml") {
-        Ok(value) => {
-            value.read_to_string(&mut s)
-        },
-        Err(..) => {
-
-        },
-    }
-
-    // f.read_line(&mut s)
-
-}
-*/
-
-use std::io::prelude::*;
-use std::fs::File;
-
-fn cat_contents(mut f: File) {
+fn file_contents(mut f: File) -> String {
     let mut contents = String::new();
 
     match f.read_to_string(&mut contents) {
         Ok(_) => { },
         Err(error) => {
-            panic!("{}", error);
+            println!("{}", error);
+            std::process::exit(1);
         },
     }
-    print!("{}", contents);
+   
+    return contents;
 }
 
 fn main() {
-    let file = File::open("Cargo.toml"); 
+    match env::args().nth(1) {
+        Some(p) => {
+            let path = Path::new(&p);
+            let file = File::open(path);
 
-    match file { 
-        Ok(f) => {
-            cat_contents(f);
+            match file { 
+                Ok(f) => {
+                    println!("{}", file_contents(f));
+                },
+                Err(error) => {
+                    println!("{}", error);
+                    std::process::exit(1);
+                }
+            };
         },
-        Err(error) => {
-            panic!("{}", error); 
+        None => {
+            println!("Usage: cat [path-to-file]");
+            std::process::exit(1);
         }
     };
 }
